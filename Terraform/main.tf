90ddf4b4a14b87cf1f183d9a758de9efa205c69d
IsaacBezares <isaacbezares@IsaacBezares-MacBook-Pro.local>
2021-12-03 18:35:49 -0600

    format terraform files

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,43 +1,42 @@
 provider "azurerm" {
   features {}
-//   skip_provider_registration = true
 }
 
 resource "azurerm_resource_group" "rg" {
-  name                = "rg-${var.suffix}-${var.environment}-${var.location}"
-  location            = var.location
+  name     = "rg-${var.suffix}-${var.environment}-${var.location}"
+  location = var.location
 }
 
 module "security" {
-  source              = "./modules/security"
-  location            = var.location
-  rg_name             = azurerm_resource_group.rg.name
-  SSH_KEY             = var.SSH_KEY
+  source   = "./modules/security"
+  location = var.location
+  rg_name  = azurerm_resource_group.rg.name
+  SSH_KEY  = var.SSH_KEY
 }
 
 module "vnet" {
-  source              = "./modules/vnet"
-  suffix              = var.suffix
-  environment         = var.environment
-  location            = var.location
-  rg_name             = azurerm_resource_group.rg.name
+  source      = "./modules/vnet"
+  suffix      = var.suffix
+  environment = var.environment
+  location    = var.location
+  rg_name     = azurerm_resource_group.rg.name
 }
 
 module "acr" {
-  source              = "./modules/acr"
-  suffix              = var.suffix
-  environment         = var.environment
-  location            = var.location
-  rg_name             = azurerm_resource_group.rg.name
+  source      = "./modules/acr"
+  suffix      = var.suffix
+  environment = var.environment
+  location    = var.location
+  rg_name     = azurerm_resource_group.rg.name
 }
 
 module "aks" {
-  source              = "./modules/aks"
-  suffix              = var.suffix
-  environment         = var.environment
-  location            = var.location
-  rg_name             = azurerm_resource_group.rg.name
-  acr_id              = module.acr.id
+  source      = "./modules/aks"
+  suffix      = var.suffix
+  environment = var.environment
+  location    = var.location
+  rg_name     = azurerm_resource_group.rg.name
+  acr_id      = module.acr.id
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
@@ -48,9 +47,9 @@ resource "azurerm_linux_virtual_machine" "vm" {
   size                  = "Standard_DS1_v2"
 
   os_disk {
-    name                    = "myOsDisk"
-    caching                 = "ReadWrite"
-    storage_account_type    = "Premium_LRS"
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
   }
 
   source_image_reference {
@@ -60,13 +59,13 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 
-  computer_name  = "azurevm"
-  admin_username = "azureuser"
+  computer_name                   = "azurevm"
+  admin_username                  = "azureuser"
   disable_password_authentication = true
 
   admin_ssh_key {
-    username       = "azureuser"
-    public_key     = module.security.public_key
+    username   = "azureuser"
+    public_key = module.security.public_key
   }
 
   tags = {
--- Terraform/terraform.tf
+++ Terraform/terraform.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   required_providers {
-      azurerm = {
-        source = "hashicorp/azurerm"
-        version = "=2.46.0"
-      }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
   }
 }
